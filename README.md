4e31a72dc878dc96d7fd54cfcd10b94f04819361
Philip Yuen <philip.yuen.13@gmail.com>
2021-03-09 12:07:07 -0500

    Project Initialization
    Philip Yuen
    Creation of project with spring boot dependencies

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CourseManagementSystem
+
 Comp3004-Team14 | A web-based Course Management System
---
+++ src/test/java/com/team14/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team14.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
